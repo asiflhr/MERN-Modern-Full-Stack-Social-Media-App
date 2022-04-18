ddad0af436e396dbfa0d3a95a2a26fd43afaa96b
Muhammad Asif <asif88383@gmail.com>
2022-04-18 11:09:27 +0500

    final backend commit

--- minterest_backend/schemas/schema.js
+++ minterest_backend/schemas/schema.js
@@ -4,6 +4,11 @@ import createSchema from 'part:@sanity/base/schema-creator'
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import user from './user'
+import pin from './pin'
+import comment from './comment'
+import save from './save'
+import postedBy from './postedBy'
+
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -12,6 +17,6 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    user,
+    user, pin, comment, save, postedBy 
   ]),
 })
