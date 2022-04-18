4477c04ec866b01962ad99b4253f5e37fa320c5c
Muhammad Asif <asif88383@gmail.com>
2022-04-18 11:01:49 +0500

    first commit

---
+++ minterest_backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default{
+    name: 'comment',
+    title: 'Comment',
+    type: 'document',
+    fields: [
+        {
+            name: 'comment',
+            title: 'Comment',
+            type: 'string',
+        },
+        {
+            name: 'postedBy',
+            title: 'PostedBy',
+            type: 'postedBy',
+        },
+    ]
+}
---
+++ minterest_backend/schemas/pin.js
@@ -0,0 +1,57 @@
+export default{
+    name: 'pin',
+    title: 'Pin',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'url',
+        },
+        {
+            name: 'about',
+            title: 'About',
+            type: 'string',
+        },
+        {
+            name: 'category',
+            title: 'Category',
+            type: 'string',
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },  // <-- now it's optional
+        },
+        {
+            name: 'userID',
+            title: 'UserID',
+            type: 'string',
+        },
+        {
+            name: 'postedBy',
+            title: 'PostedBy',
+            type: 'postedBy',
+        },
+        {
+            name: 'save',
+            title: 'Save',
+            type: 'array',
+            of: [{type: 'save'}],
+        },
+        {
+            name: 'comments',
+            title: 'Comments',
+            type: 'array',
+            of: [{type: 'comment'}],
+        },
+    ]
+}
