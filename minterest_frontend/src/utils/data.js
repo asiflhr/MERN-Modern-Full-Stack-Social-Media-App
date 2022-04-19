76ab113b3266241c5eb8f6a6540356e801b45463
Muhammad Asif <asif88383@gmail.com>
2022-04-19 23:37:17 +0500

    first commit

--- minterest_frontend/src/utils/data.js
+++ minterest_frontend/src/utils/data.js
@@ -152,6 +152,32 @@ export const pinDetailMorePinQuery = (pin) => {
   return query;
 };
 
+export const searchQuery = (searchTerm) => {
+  const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
+          image{
+            asset->{
+              url
+            }
+          },
+              _id,
+              destination,
+              postedBy->{
+                _id,
+                userName,
+                image
+              },
+              save[]{
+                _key,
+                postedBy->{
+                  _id,
+                  userName,
+                  image
+                },
+              },
+            }`;
+  return query;
+};
+
 export const userQuery = (userId) => {
   const query = `*[_type == "user" && _id == "${userId}"]`;
   return query;
