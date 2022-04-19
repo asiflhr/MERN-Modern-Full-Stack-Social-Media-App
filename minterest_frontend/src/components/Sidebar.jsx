fb8ce818de03c477a9e41709ab7158d0f08c2207
Muhammad Asif <asif88383@gmail.com>
2022-04-19 23:27:37 +0500

    first commit

--- minterest_frontend/src/components/Sidebar.jsx
+++ minterest_frontend/src/components/Sidebar.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { RiHomeFill } from 'react-icons/ri';
+import { IoIosArrowForward } from 'react-icons/io';
+import logo from '../assets/logo.png';
+import { categories } from '../utils/data';
 
 const Sidebar = () => {
   return (
--- minterest_frontend/src/container/Home.jsx
+++ minterest_frontend/src/container/Home.jsx
@@ -1,19 +1,17 @@
-import React, {useState, useRef, useEffect} from 'react'
-import {HiMenu} from 'react-icons/hi'
-import {AiFillCloseCircle} from 'react-icons/ai'
-import {Link, Route, Routes} from 'react-router-dom'
+import React, { useState, useRef, useEffect } from 'react';
+import { HiMenu } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, Route, Routes } from 'react-router-dom';
 
-import {Sidebar, UserProfile} from '../components'
+import { Sidebar, UserProfile } from '../components';
 import { userQuery } from '../utils/data';
-import { client } from '../client'
-import logo from '../assets/logo.png'
-import Pins from './Pins'
-
+import { client } from '../client';
+import Pins from './Pins';
+import logo from '../assets/logo.png';
 
 const Home = () => {
-
-  const [toggleSidebar, setToggleSidebar] = useState(false)
-  const [user, setUser] = useState(null);
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const [user, setUser] = useState();
   const scrollRef = useRef(null);
 
   const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
@@ -26,7 +24,7 @@ const Home = () => {
     });
   }, []);
 
-  useEffect(() => { 
+  useEffect(() => {
     scrollRef.current.scrollTo(0, 0);
   });
 
@@ -61,7 +59,7 @@ const Home = () => {
         </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
