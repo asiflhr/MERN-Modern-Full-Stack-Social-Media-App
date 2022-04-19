b071b9f3bec7da9d99bbfef080b7fc4f5c9a6a84
Muhammad Asif <asif88383@gmail.com>
2022-04-19 23:47:51 +0500

    first commit

---
+++ minterest_frontend/src/components/MasonryLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MasonryLayout = () => {
+  return (
+    <div>MasonryLayout</div>
+  )
+}
+
+export default MasonryLayout
---
+++ minterest_frontend/src/components/Pin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pin = () => {
+  return (
+    <div>Pin</div>
+  )
+}
+
+export default Pin
