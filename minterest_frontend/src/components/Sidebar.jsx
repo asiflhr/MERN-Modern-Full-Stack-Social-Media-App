3bbc1bd1ce84d35f93a48b8cb14e19afef4358bf
Muhammad Asif <asif88383@gmail.com>
2022-04-19 23:29:45 +0500

    first commit

--- minterest_frontend/src/components/Sidebar.jsx
+++ minterest_frontend/src/components/Sidebar.jsx
@@ -5,7 +5,14 @@ import { IoIosArrowForward } from 'react-icons/io';
 import logo from '../assets/logo.png';
 import { categories } from '../utils/data';
 
-const Sidebar = () => {
+const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
+const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize';
+
+const Sidebar = ({ closeToggle, user }) => {
+  const handleCloseSidebar = () => {
+    if (closeToggle) closeToggle(false);
+  };
+
   return (
     <div>Sidebar</div>
   )
