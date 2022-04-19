9bb79b0aa309896dfd68fa556f8e6496d2d3191a
Muhammad Asif <asif88383@gmail.com>
2022-04-19 16:10:53 +0500

    first commit

--- minterest_frontend/src/container/Home.jsx
+++ minterest_frontend/src/container/Home.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useState, useRef, useEffect} from 'react'
+import {HiMenu} from 'react-icons/hi'
+import {AiFillCloseCircle} from 'react-icons/ai'
+import {Link, Route, Routes} from 'react-router-dom'
+
+import {Sidebar, UserProfile} from '../components'
+import { client } from '../client'
+import logo from '../assets/logo.png'
+import Pins from './Pins'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
+      <div className='hidden md:flex h-screen flex-initial'>
+
+      </div>
+    </div>
   )
 }
 
