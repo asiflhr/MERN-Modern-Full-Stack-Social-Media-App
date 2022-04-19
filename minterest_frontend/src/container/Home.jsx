0c1f2d93dd3219db2d7cc25a522e07dee3276a63
Muhammad Asif <asif88383@gmail.com>
2022-04-19 16:22:09 +0500

    first commit

--- minterest_frontend/src/container/Home.jsx
+++ minterest_frontend/src/container/Home.jsx
@@ -10,10 +10,39 @@ import Pins from './Pins'
 
 
 const Home = () => {
+
+  const [toggleSiebar, setToggleSiebar] = useState(false)
+  const [user, setUser] = useState();
+  const scrollRef = useRef(null);
+
+  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+  // useEffect(() => {
+  //   const query = userQuery(userInfo?.googleId);
+
+  //   client.fetch(query).then((data) => {
+  //     setUser(data[0]);
+  //   });
+  // }, []);
+
+  useEffect(() => {
+    scrollRef.current.scrollTo(0, 0);
+  });
+
   return (
     <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
       <div className='hidden md:flex h-screen flex-initial'>
+        <Sidebar />
+      </div>
 
+      <div className='flex md:hidden flex-row'>
+        <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSiebar(false)} />
+        <Link to='/'>
+        <img src={logo} alt='logo' className='w-32' />
+      </Link>
+      <Link to={`user-profile/${user?._id}`}>
+        <img src={''} alt='profile' className='w-32' />
+      </Link>
       </div>
     </div>
   )
