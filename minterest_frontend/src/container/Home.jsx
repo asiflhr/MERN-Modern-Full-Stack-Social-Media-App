0cd6e268867d4efe47cfe8d5bf9fece9df8d67ae
Muhammad Asif <asif88383@gmail.com>
2022-04-19 16:37:45 +0500

    first commit

--- minterest_frontend/src/container/Home.jsx
+++ minterest_frontend/src/container/Home.jsx
@@ -4,6 +4,7 @@ import {AiFillCloseCircle} from 'react-icons/ai'
 import {Link, Route, Routes} from 'react-router-dom'
 
 import {Sidebar, UserProfile} from '../components'
+import { userQuery } from '../utils/data';
 import { client } from '../client'
 import logo from '../assets/logo.png'
 import Pins from './Pins'
@@ -11,39 +12,47 @@ import Pins from './Pins'
 
 const Home = () => {
 
-  const [toggleSiebar, setToggleSiebar] = useState(false)
-  const [user, setUser] = useState();
+  const [toggleSidebar, setToggleSidebar] = useState(false)
+  const [user, setUser] = useState(null);
   const scrollRef = useRef(null);
 
   const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
 
-  // useEffect(() => {
-  //   const query = userQuery(userInfo?.googleId);
-
-  //   client.fetch(query).then((data) => {
-  //     setUser(data[0]);
-  //   });
-  // }, []);
-
   useEffect(() => {
-    scrollRef.current.scrollTo(0, 0);
-  });
+    const query = userQuery(userInfo?.googleId);
+
+    client.fetch(query).then((data) => {
+      setUser(data[0]);
+    });
+  }, []);
+
+  // useEffect(() => {
+  //   scrollRef.current.scrollTo(0, 0);
+  // });
 
   return (
     <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
       <div className='hidden md:flex h-screen flex-initial'>
-        <Sidebar />
+        <Sidebar user={user && user} />
       </div>
 
       <div className='flex md:hidden flex-row'>
-        <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSiebar(false)} />
+        <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(true)} />
         <Link to='/'>
         <img src={logo} alt='logo' className='w-32' />
       </Link>
       <Link to={`user-profile/${user?._id}`}>
-        <img src={''} alt='profile' className='w-32' />
+        <img src={user?.image} alt='profile' className='w-32' />
       </Link>
       </div>
+      {toggleSidebar && (
+        <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+          <div className="absolute w-full flex justify-end items-center p-2">
+            <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
+          </div>
+          <Sidebar closeToggle={setToggleSidebar} user={user && user} />
+        </div>
+        )}
     </div>
   )
 }
