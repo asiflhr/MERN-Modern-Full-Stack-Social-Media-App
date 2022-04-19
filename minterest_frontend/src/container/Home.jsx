49b2a1bed098fab554af415f26214858a34794ec
Muhammad Asif <asif88383@gmail.com>
2022-04-19 16:44:39 +0500

    first commit

--- minterest_frontend/src/container/Home.jsx
+++ minterest_frontend/src/container/Home.jsx
@@ -26,26 +26,26 @@ const Home = () => {
     });
   }, []);
 
-  // useEffect(() => {
-  //   scrollRef.current.scrollTo(0, 0);
-  // });
+  useEffect(() => { 
+    scrollRef.current.scrollTo(0, 0);
+  });
 
   return (
-    <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
-      <div className='hidden md:flex h-screen flex-initial'>
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
+      <div className="hidden md:flex h-screen flex-initial">
         <Sidebar user={user && user} />
       </div>
-
-      <div className='flex md:hidden flex-row'>
-        <HiMenu fontSize={40} className='cursor-pointer' onClick={() => setToggleSidebar(true)} />
-        <Link to='/'>
-        <img src={logo} alt='logo' className='w-32' />
-      </Link>
-      <Link to={`user-profile/${user?._id}`}>
-        <img src={user?.image} alt='profile' className='w-32' />
-      </Link>
-      </div>
-      {toggleSidebar && (
+      <div className="flex md:hidden flex-row">
+        <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+          <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(true)} />
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-28" />
+          </Link>
+          <Link to={`user-profile/${user?._id}`}>
+            <img src={user?.image} alt="user-pic" className="w-9 h-9 rounded-full " />
+          </Link>
+        </div>
+        {toggleSidebar && (
         <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
           <div className="absolute w-full flex justify-end items-center p-2">
             <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
@@ -53,6 +53,13 @@ const Home = () => {
           <Sidebar closeToggle={setToggleSidebar} user={user && user} />
         </div>
         )}
+      </div>
+      <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+        <Routes>
+          <Route path="/user-profile/:userId" element={<UserProfile />} />
+          <Route path="/*" element={<Pins user={user && user} />} />
+        </Routes>
+      </div>
     </div>
   )
 }
