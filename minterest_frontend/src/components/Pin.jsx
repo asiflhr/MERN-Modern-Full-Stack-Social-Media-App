9fc5a3f19bde8a6f03d9b969cd30fc83343eae33
Muhammad Asif <asif88383@gmail.com>
2022-04-20 11:54:17 +0500

    first commit

--- minterest_frontend/src/components/Pin.jsx
+++ minterest_frontend/src/components/Pin.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import { MdDownloadForOffline } from 'react-icons/md';
+import { AiTwotoneDelete } from 'react-icons/ai';
+import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
+
+import { client, urlFor } from '../client';
 
 const Pin = () => {
   return (
