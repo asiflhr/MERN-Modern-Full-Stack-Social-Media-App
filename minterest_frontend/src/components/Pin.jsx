dc51630b41feb3ba818cdb18b8bb7ab1870e9f74
Muhammad Asif <asif88383@gmail.com>
2022-04-20 11:55:23 +0500

    first commit

--- minterest_frontend/src/components/Pin.jsx
+++ minterest_frontend/src/components/Pin.jsx
@@ -7,7 +7,57 @@ import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
 
 import { client, urlFor } from '../client';
 
-const Pin = () => {
+const Pin = ({pin}) => {
+
+  const [postHovered, setPostHovered] = useState(false);
+  const [savingPost, setSavingPost] = useState(false);
+
+  const navigate = useNavigate();
+
+  const { postedBy, image, _id, destination } = pin;
+
+  const user =
+    localStorage.getItem("user") !== "undefined"
+      ? JSON.parse(localStorage.getItem("user"))
+      : localStorage.clear();
+
+  const deletePin = (id) => {
+    client.delete(id).then(() => {
+      window.location.reload();
+    });
+  };
+
+  let alreadySaved = pin?.save?.filter(
+    (item) => item?.postedBy?._id === user?.googleId
+  );
+
+  alreadySaved = alreadySaved?.length > 0 ? alreadySaved : [];
+
+  const savePin = (id) => {
+    if (alreadySaved?.length === 0) {
+      setSavingPost(true);
+
+      client
+        .patch(id)
+        .setIfMissing({ save: [] })
+        .insert("after", "save[-1]", [
+          {
+            _key: uuidv4(),
+            userId: user?.googleId,
+            postedBy: {
+              _type: "postedBy",
+              _ref: user?.googleId,
+            },
+          },
+        ])
+        .commit()
+        .then(() => {
+          window.location.reload();
+          setSavingPost(false);
+        });
+    }
+  };
+
   return (
     <div>Pin</div>
   )
