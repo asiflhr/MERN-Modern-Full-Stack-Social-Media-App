e7ce8f4b0846153f92452ce03f509930316e9aff
Muhammad Asif <asif88383@gmail.com>
2022-04-20 15:21:24 +0500

    first commit

--- minterest_frontend/src/components/PinDetail.jsx
+++ minterest_frontend/src/components/PinDetail.jsx
@@ -1,6 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { MdDownloadForOffline } from 'react-icons/md';
+import { Link, useParams } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
-const PinDetail = () => {
+import { client, urlFor } from '../client';
+import MasonryLayout from './MasonryLayout';
+import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data';
+import Spinner from './Spinner';
+
+const PinDetail = ({user}) => {
+  const { pinId } = useParams();
+  const [pins, setPins] = useState();
+  const [pinDetail, setPinDetail] = useState();
+  const [comment, setComment] = useState("");
+  const [addingComment, setAddingComment] = useState(false);
+
+  const fetchPinDetails = () => {
+    const query = pinDetailQuery(pinId);
+
+    if (query) {
+      client.fetch(`${query}`).then((data) => {
+        setPinDetail(data[0]);
+        console.log(data);
+        if (data[0]) {
+          const query1 = pinDetailMorePinQuery(data[0]);
+          client.fetch(query1).then((res) => {
+            setPins(res);
+          });
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchPinDetails();
+  }, [pinId]);
+
+  const addComment = () => {
+    if (comment) {
+      setAddingComment(true);
+
+      client
+        .patch(pinId)
+        .setIfMissing({ comments: [] })
+        .insert("after", "comments[-1]", [
+          {
+            comment,
+            _key: uuidv4(),
+            postedBy: { _type: "postedBy", _ref: user._id },
+          },
+        ])
+        .commit()
+        .then(() => {
+          fetchPinDetails();
+          setComment("");
+          setAddingComment(false);
+        });
+    }
+  };
+
+  if (!pinDetail) {
+    return <Spinner message="Showing pin" />;
+  }
+
   return (
     <div>PinDetail</div>
   )
