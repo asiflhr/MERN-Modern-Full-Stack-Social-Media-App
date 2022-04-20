18e95dca76dc09903b44be5acba41a20d8149786
Muhammad Asif <asif88383@gmail.com>
2022-04-20 12:41:59 +0500

    first commit

--- minterest_frontend/src/components/CreatePin.jsx
+++ minterest_frontend/src/components/CreatePin.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
+import { MdDelete } from 'react-icons/md';
 
-const CreatePin = () => {
+import { categories } from '../utils/data';
+import { client } from '../client';
+import Spinner from './Spinner';
+
+const CreatePin = ({ user }) => {
+  const [title, setTitle] = useState('');
+  const [about, setAbout] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [destination, setDestination] = useState();
+  const [fields, setFields] = useState();
+  const [category, setCategory] = useState();
+  const [imageAsset, setImageAsset] = useState();
+  const [wrongImageType, setWrongImageType] = useState(false);
+
+  const navigate = useNavigate();
+
+  
   return (
     <div>CreatePin</div>
   )
