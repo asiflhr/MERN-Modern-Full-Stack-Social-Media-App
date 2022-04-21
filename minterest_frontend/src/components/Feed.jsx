e19e8ded4d55b205b8e02847ee4c5ebf7747452b
Muhammad Asif <asif88383@gmail.com>
2022-04-21 16:59:04 +0500

    first commit

--- minterest_frontend/src/components/Feed.jsx
+++ minterest_frontend/src/components/Feed.jsx
@@ -34,9 +34,7 @@ const Feed = () => {
       <Spinner message={`We are adding ${ideaName} ideas to your feed!`} />
     );
   }
-  if(!pins.length) {
-    return <h2>No Pins Available</h2>;
-  }
+  
   return (
     <div>
       {pins && (
