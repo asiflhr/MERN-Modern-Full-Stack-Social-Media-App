dc9bde8318362b3d6c572ccb97a1fcee759eb0f1
Muhammad Asif <asif88383@gmail.com>
2022-05-04 04:07:48 +0500

    first commit

--- minterest_frontend/src/index.js
+++ minterest_frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter as Router} from 'react-router-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
   <Router>
     <App />
-  </Router>
+  </Router>,
+  document.getElementById('root'),
 );
